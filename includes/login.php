0c4564ba48f22eede9d8770bc0e8b0edfdd66433
= <=>
2020-01-18 15:09:31 +0700

    tambah view add user

--- includes/login.php
+++ includes/login.php
@@ -8,17 +8,21 @@ Flight::route('POST /login', function(){
     $password = $_POST['password'];
 
     // cek di database dan login / redirect kalo ga terdaftar
-    $exist = $username == 'user';
+    $db = Flight::db();
+    $db->where('username', $username);
+    $db->where('password', md5( $password ) );
+    $users = $db->get('users');
+
+    $exist = !empty( $users );
     if ( $exist ) {
-    	// logged in
-    	$_SESSION['user'] = 'user';
-    	Flight::redirect( '/' );
+        // logged in
+        $_SESSION['user'] = $users[0]['username'];
+        Flight::redirect( '/' );
     } else {
-    	// kembalikan ke hlaman login
-    	Flight::redirect( '/login' );
+        // kembalikan ke hlaman login
+        Flight::redirect( '/login' );
     }
 });
-
 Flight::route( '/logout', function(){
 	if ( isset( $_SESSION['user'] ) ){
 		unset( $_SESSION['user'] );
--- includes/users.php
+++ includes/users.php
@@ -1,17 +1,27 @@
 <?php
 Flight::route( 'POST /users/add', function(){
 	$db = Flight::db();
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+	
+	$data = array(
+		'username' => $username,
+		'password' => md5( $password )
+	 );
 
-	// $data = array(
-	// 	'username' => 'somenone',
-	// 	'password' => md5('password')
-	// );
+		$id = $db->insert('users', $data);
+	if ($id){
+	    Flight::redirect( get_url( 'users' ) );
+	} else {
+	    echo 'insert failed: ' . $db->getLastError();
+	}
+});
 
-	// $id = $db->insert('users', $data);
-	// if ($id)
-	//     echo 'user was created. Id=' . $id;
-	// else
-	//     echo 'insert failed: ' . $db->getLastError();
+
+Flight::route('GET /users/add', function(){
+	Flight::view()->set('title', 'Add');
+	Flight::render('add-user');
 });
 
 Flight::route( 'GET /users(/page/@page:[0-9]+)', function($page){
